b7dfbdeaf4837f74c3bbffe8906a753fe12b8511
Ayndpa <abcxiaoyao1234@163.com>
2026-06-06 17:42:29 +0800

    修复设置无法持久化保存的问题

--- framework.js
+++ framework.js
@@ -1,13 +1,14 @@
 maplebirch.tool.onInit(() => {
-  V.noBusHarassment ??= {};
-  if (V.noBusHarassment.normal === undefined) {
-    V.noBusHarassment.normal = true;
+  V.options ??= {};
+  V.options.noBusHarassment ??= {};
+  if (V.options.noBusHarassment.normal === undefined) {
+    V.options.noBusHarassment.normal = true;
   }
-  if (V.noBusHarassment.pregnancy === undefined) {
-    V.noBusHarassment.pregnancy = true;
+  if (V.options.noBusHarassment.pregnancy === undefined) {
+    V.options.noBusHarassment.pregnancy = true;
   }
-  if (V.noBusHarassment.passout === undefined) {
-    V.noBusHarassment.passout = true;
+  if (V.options.noBusHarassment.passout === undefined) {
+    V.options.noBusHarassment.passout = true;
   }
 });
 
